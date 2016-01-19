d2df7548d094a5516537d3af88a986ae074e4dff
Aundra Miller and Mollie Bootsma <student@epicodus.com>
2016-01-19 09:16:54 -0800

    add UI logic and refactored specs

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ var leapYear = function(year) {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
