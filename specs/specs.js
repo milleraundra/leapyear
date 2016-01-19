b6c40602fbf53d7eee3f86903726ceaca710a6a8
Aundra Miller and Mollie Bootsma <student@epicodus.com>
2016-01-19 09:01:15 -0800

    add condition for divisibility by 400

--- specs/specs.js
+++ specs/specs.js
@@ -6,4 +6,12 @@ describe ('leapYear', function() {
   it("is true for years divisible by 4", function() {
     expect(leapYear(2004)).to.equal(true);
   });
+
+  it("is false for years divisible by 100", function() {
+    expect(leapYear(1900)).to.equal(false);
+  });
+
+  it("is true for years divisible by 400", function() {
+    expect(leapYear(2000)).to.equal(true);
+  });
 });
